d5b5bc2eaa92732ae988e45a5247cfae69863f2f
root <root@pc>
2023-04-04 13:57:16 +0200

    add another route that display “HBNB”

---
+++ web_flask/1-hbnb_route.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""script that starts a Flask web application"""
+from flask import Flask
+
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def Hello_HBNB():
+    """ display “Hello HBNB!” """
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb', strict_slashes=False)
+def HBNB():
+    """ display “HBNB” """
+    return "HBNB"
+
+
+if __name__ == '__main__':
+    app.run(port=5000, host='0.0.0.0')
