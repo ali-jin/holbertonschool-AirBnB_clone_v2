d10645215cec51ac558293a159524ab4fba2f22b
root <root@pc>
2023-04-06 14:28:53 +0200

    add jinja for the filter on the airbnb clone site

---
+++ web_flask/10-hbnb_filters.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""script that starts a Flask web application"""
+from flask import render_template
+from flask import Flask
+from models import storage
+from models.state import State
+from models.amenity import Amenity
+
+
+app = Flask(__name__)
+
+
+@app.teardown_appcontext
+def close_session(self):
+    """remove the current SQLAlchemy Session"""
+    storage.close()
+
+@app.route('/hbnb_filters', strict_slashes=False)
+def display_filter():
+    states = storage.all(State)
+    amenities = storage.all(Amenity)
+    return render_template('10-hbnb_filters.html', states=states, amenities=amenities)
+
+
+if __name__ == '__main__':
+    app.run(port=5000, host='0.0.0.0')
