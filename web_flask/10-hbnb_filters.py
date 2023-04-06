041cedf237b43478ed12fac13fbd1bca23e3bcc7
root <root@pc>
2023-04-06 15:39:19 +0200

    fix syntax and pycodestyle

--- web_flask/10-hbnb_filters.py
+++ web_flask/10-hbnb_filters.py
@@ -15,12 +15,15 @@ def close_session(self):
     """remove the current SQLAlchemy Session"""
     storage.close()
 
+
 @app.route('/hbnb_filters', strict_slashes=False)
 def display_filter():
+    """display the filter"""
     states = storage.all(State)
     amenities = storage.all(Amenity)
-    return render_template('10-hbnb_filters.html', states=states, amenities=amenities)
+    return render_template('10-hbnb_filters.html',
+                           states=states, amenities=amenities)
 
 
 if __name__ == '__main__':
-    app.run(port=5000, host='0.0.0.0')
+    app.run(host='0.0.0.0', port=5000)
